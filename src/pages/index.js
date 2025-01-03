253907dd7bb86684c73fc8ff7b506eec9a045de8
Basuki Rahmad <Farhan Hisbullah A'isyi Basuki>
2025-01-03 22:39:32 +0700

    fixing so many bugsssss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
-    return (
-      <div>
-        <h1>Welcome to My Next.js App!</h1>
-      </div>
-    );
-  }
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/login");
+  }, [router]);
+
+  return null;
+}
